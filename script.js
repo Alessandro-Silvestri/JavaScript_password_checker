4ad3a0c1c00ed012a5a05f8910a2ce3804392187
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2025-07-28 10:10:48 +0100

    added 'password ok' fixed bug: capital/no capital

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-// at least 12 characters
-// capital and not capital char
-// at least a special char
-// at least a number
-// no spaces inside 
-// see password function
+// BUGS:
+// empty fields
 
 // objects
 let pass1 = document.getElementById("pass1");
@@ -12,11 +8,13 @@ let see1 = document.getElementById("see1");
 let see2 = document.getElementById("see2");
 let requirements = document.getElementsByClassName("requirements");
 let buttonCheckPassword = document.getElementById("buttonCheckPassword");
+let p_passwordOk = document.getElementById("p_passwordOk");
 
 // all the requirements in red
 for (let i = 0; i < requirements.length; i++) {
-    console.log(requirements[i].style.color = "red");
+    requirements[i].style.color = "red";
 }
+
 
 // "see" button behavior (changing the "type" attribute: password/text)
 see1.addEventListener("click", () => {
@@ -33,6 +31,8 @@ see2.addEventListener("focusout", () => {
     pass2.attributes[1].value = "password";
 })
 
+// hide password OK
+p_passwordOk.style.display = "none";
 
 // behaviors: every requirement is solved as a function
 function IsMore12(pass) {
@@ -43,13 +43,14 @@ function IsCapNoCap(pass) {
     let isUpper = false;
     let isLower = false;
     for (let char of pass) {
-        if (char === char.toUpperCase())
-            isUpper = true;
-        if (char === char.toLowerCase()) {
-            isLower = true;
-        }
-        if (isUpper && isLower) {
-            break;
+        // this loop avoids the numbers 
+        if (isNaN(char)) {
+            if (char === char.toUpperCase())
+                isUpper = true;
+            if (char === char.toLowerCase())
+                isLower = true;
+            if (isUpper && isLower)
+                break;
         }
     }
     return isUpper && isLower;
@@ -70,7 +71,6 @@ function isSpecialCharInside(pass) {
 }
 
 function isDigitInside(pass) {
-    debugger;
     let isDigit = false;
     for (let char of pass) {
         // if the character is a number
@@ -86,17 +86,51 @@ function isSpaceInside(pass) {
     return pass.includes(" ");
 }
 
+function equalPws(pass1, pass2) {
+    return pass1 === pass2;
+}
 
+function checkAllGreen() {
+    let allGreen = true;
+    for (let i = 0; i < requirements.length; i++) {
+        if (requirements[i].style.color === "red") {
+            allGreen = false;
+            break;
+        }
+    }
+    return allGreen;
+}
 
 buttonCheckPassword.addEventListener("click", () => {
     let pw1 = pass1.value;
     let pw2 = pass2.value;
-    if (IsMore12(pw1)) {
+    if (IsMore12(pw1))
         requirements[0].style.color = "green";
-    }
-    else {
+    else
         requirements[0].style.color = "red";
-
-    }
+    if (IsCapNoCap(pw1))
+        requirements[1].style.color = "green";
+    else
+        requirements[1].style.color = "red";
+    if (isSpecialCharInside(pw1))
+        requirements[2].style.color = "green";
+    else
+        requirements[2].style.color = "red";
+    if (isDigitInside(pw1))
+        requirements[3].style.color = "green";
+    else
+        requirements[3].style.color = "red";
+    if (!isSpaceInside(pw1))
+        requirements[4].style.color = "green";
+    else
+        requirements[4].style.color = "red";
+    if (equalPws(pw1, pw2))
+        requirements[5].style.color = "green";
+    else
+        requirements[5].style.color = "red";
+    if (checkAllGreen())
+        p_passwordOk.style.display = "";
+    else
+        p_passwordOk.style.display = "none";
 })
 
